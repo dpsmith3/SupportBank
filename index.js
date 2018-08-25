4c7e43a499f3630a65af477fe5ceb9ff177bb564
dpsmith3 <dpsmith3@outlook.com>
2018-08-25 21:17:09 +0100

    Part 1: Reading CSV files

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const readlineSync = require('readline-sync');
 const moment = require('moment');
 const fs = require('fs');
 
-
 class Person {
     constructor(name, account) {
         this.name = name;
@@ -11,8 +10,8 @@ class Person {
 
     displayPerson() {
         const result = `
-        Name: ${this.name}
-        Account: ${this.account.toFixed(2)}`;
+Name: ${this.name}
+Account: ${this.account.toFixed(2)}`;
         console.log(result);
     }
 }
@@ -28,7 +27,7 @@ class Transaction {
 
     displayTransaction() {
         const result = `
-Date: ${this.date}
+Date: ${this.date.format("dddd, MMMM Do YYYY")}
 From: ${this.from}
 To: ${this.to}
 Narrative: ${this.narrative}
@@ -39,24 +38,24 @@ Amount: ${this.amount}`;
 
 function parseCsvTransactions(data) {
     return data.split('\r\n')
-    .map(x => x.split(','))
-    .slice(1).map(x => new Transaction(moment(x[0], "DD-MM-YYYY"), x[1], x[2], x[3], +`${x[4]}`)
-    )
+        .map(x => x.split(','))
+        .slice(1).map(x => new Transaction(moment(x[0], "DD-MM-YYYY"), x[1], x[2], x[3], +`${x[4]}`)
+        )
 }
 
 function updatePersons(persons, transactions) {
     transactions.forEach(transaction => {
-        //HANDLE NEW PERSONS
-        if (! persons.map(elem => elem.name).includes(transaction.from)) {
-            persons.push(new Person(transaction.from, transaction.amount));
-        } else if (! persons.map(elem => elem.name).includes(transaction.to)) {
+        // Handle new persons
+        if (!persons.map(elem => elem.name).includes(transaction.from)) {
+            persons.push(new Person(transaction.from, - transaction.amount));
+        } else if (!persons.map(elem => elem.name).includes(transaction.to)) {
             persons.push(new Person(transaction.to, transaction.amount));
         } else {
-            //HANDLE EXISTING PERSONS by finding person in persons array and then updating amount
+            //Handle existing persons
             persons.forEach(person => {
-                if (person.name == transaction.from) {
+                if (person.name === transaction.from) {
                     person.account -= transaction.amount
-                } else if (person.name == transaction.to) {
+                } else if (person.name === transaction.to) {
                     person.account += transaction.amount
                 }
             })
@@ -64,41 +63,55 @@ function updatePersons(persons, transactions) {
     });
     return persons;
 }
-        
-let persons = [];
-const rawTransactions2014 = fs.readFileSync("./transactions2014.csv", "utf8");
-const transactions2014 = parseCsvTransactions(rawTransactions2014);
-const persons2014 = updatePersons(persons, transactions2014);
+
+function listAll(persons) {
+    persons.forEach(element => element.displayPerson());
+}
+
+function listAccount(command, persons, transactions) {
+    const requestedName = command.slice(5);
+    const requestedAccount = persons.find(person => person.name === requestedName).account.toFixed(2);
+    const requestedTransactions = transactions.filter(transaction => {
+        if (transaction.from === requestedName || transaction.to === requestedName) {
+            return true;
+        }
+    });
+
+    if (requestedAccount > 0) {
+        console.log(`\n${requestedName} is owed ${requestedAccount}`);
+    } else {
+        console.log(`\n${requestedName} owes ${Math.abs(requestedAccount)}`);
+    }
+    console.log(`Here are ${requestedName}'s transactions:`);
+    requestedTransactions.forEach(transaction => transaction.displayTransaction());
+}
 
 // Main
-console.log(`============================
-Welcome to the Support Bank!`);
+let persons = [];
+const rawTransactions2014 = fs.readFileSync("./transactions2014.csv", "utf8");
+const transactions = parseCsvTransactions(rawTransactions2014);
+persons = updatePersons(persons, transactions);
+
+console.log(`\n
+Welcome to the Support Bank!
+============================`);
+
 while (true) {
-    console.log(`============================
+    console.log(`
 Please enter one of the following commands:
 
-    List Transactions  [To see a list of all transactions]
     List All           [To see a list of all people and what they owe or are owed]
     List [Account]     [To see a particular person's transactions and their amount owed or owing]
 
 or CTRL + C to close the program`);
-    
-    var command = readlineSync.prompt();
-    
-    if (command === 'List Transactions') {
-        transactions2014.forEach(element => element.displayTransaction());
-    } else if (command === 'List All') {
-        persons2014.forEach(element => element.displayPerson());
+
+    const command = readlineSync.prompt();
+
+    if (command === 'List All') {
+        listAll(persons);
+    } else if (command.slice(0,4) === 'List') {
+        listAccount(command, persons, transactions);
     } else {
-        const name = command.slice(5);
-        const account = persons.find(person => person.name === name).account.toFixed(2);
-        const requestedPersonsTransactions = transactions2014.filter(transaction => {if (transaction.from === name || transaction.to === name) {return true}});
-        if (account > 0) {
-            console.log(`${name} is owed ${account}`);
-        } else {
-            console.log(`${name} owes ${Math.abs(account)}`);
-        }
-        console.log(`Here are ${name}'s transactions: \n`);
-        requestedPersonsTransactions.forEach(transaction => transaction.displayTransaction());
-        }
+        console.log("\nThat command was not recognised.");
+    }
 }
