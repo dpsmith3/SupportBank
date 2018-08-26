d50fa5f37698598d5c67d2d67b6598f0a818d13b
dpsmith3 <dpsmith3@outlook.com>
2018-08-26 17:12:29 +0100

    Pulled out a module to load CSV data, and improved logging while parsing

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const moment = require('moment');
 const fs = require('fs');
 const log4js = require('log4js');
 const logger = log4js.getLogger('index');
+const loadCsvData = require('./loadCsvData.js');
 
 log4js.configure({
     appenders: {
@@ -27,63 +28,6 @@ Name: ${this.name}
 Account: ${this.account.toFixed(2)}`;
         console.log(result);
     }
-}
-
-class Transaction {
-    constructor(date, from, to, narrative, amount) {
-        this.date = date;
-        this.from = from;
-        this.to = to;
-        this.narrative = narrative;
-        this.amount = amount;
-    }
-
-    displayTransaction() {
-        const result = `
-Date: ${this.date.format("dddd, MMMM Do YYYY")}
-From: ${this.from}
-To: ${this.to}
-Narrative: ${this.narrative}
-Amount: ${this.amount}`;
-        console.log(result);
-    }
-}
-
-function parseCsvTransactions(data) {
-    const result = data.split('\r\n')
-        .map(x => x.split(','))
-        .slice(1).map(x => {
-            let transaction = new Transaction();
-            transaction.date = moment(x[0], "DD-MM-YYYY");
-            if (!moment(transaction.date).isValid()) {
-                logger.error(`'${x[0]}' could not be parsed as a date`);
-            }
-            transaction.from = x[1];
-            transaction.to = x[2];
-            transaction.narrative = x[3];
-            transaction.amount = +`${x[4]}`;
-            if (Number.isNaN(x[4])) {
-                logger.error(`${x[4]} is not a number`)
-            }
-            return transaction;
-        });
-    return result;
-}
-
-function loadTransactions(folderPath) {
-    logger.info(`Loading transactions from ${folderPath}`);
-    var allTransactions = [];
-    const filenames = fs.readdirSync(folderPath);
-    filenames.forEach(filename => {
-        logger.info(`Loading ${folderPath}/${filename}`);
-        const rawTransactions = fs.readFileSync(`./transactions/${filename}`, "utf8");
-        const transactions = parseCsvTransactions(rawTransactions);
-        transactions.forEach(transaction => {
-            allTransactions.push(transaction);
-        })
-    })
-    return allTransactions;
-    ;
 }
 
 function updatePersons(persons, transactions) {
@@ -131,7 +75,7 @@ function listAccount(command, persons, transactions) {
 
 // Main
 
-const transactions = loadTransactions('./transactions');
+const transactions = loadCsvData.loadTransactions('./transactions');
 
 let persons = [];
 persons = updatePersons(persons, transactions);
